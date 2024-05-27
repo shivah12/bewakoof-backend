69acf8eb0ea4fb2f6c0e190c05187b713309be7c
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-27 23:21:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://bewakoof-frontend.onrender.com/",
-  credentials: true, //included credentials as true
+  origin: "https://bewakoof-frontend.onrender.com", // No trailing slash
+  credentials: true, // Included credentials as true
 };
 
 app.use(cors(corsOptions));
+
+// Optionally set headers for all routes
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://bewakoof-frontend.onrender.com"); // No trailing slash
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("App working");
