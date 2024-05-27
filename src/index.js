72445e90143af0145b522812f795765ef301aac7
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-27 23:22:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-  origin: "https://bewakoof-frontend.onrender.com/",
-  credentials: true, //included credentials as true
+  origin: "https://bewakoof-frontend.onrender.com", // No trailing slash
+  credentials: true, // Included credentials as true
 };
+
 app.use(cors(corsOptions));
 
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "https://bewakoof-frontend.onrender.com"); // No trailing slash
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
